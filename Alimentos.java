731ca7b2e9059e2bfd5d972f13222782b2a79402
Israel Antunes Neto <israneto20@gmail.com>
2023-10-24 20:29:51 -0300

    V1

---
+++ Alimentos.java
@@ -0,0 +1,28 @@
+package Model;
+
+public class Alimenticio  extends Produto{
+	private String dataValidade;
+	private String ingredientes;
+	
+	public Alimenticio(String nome, double preco, String descricao, int estoque, String fabricante, String dataValidade, String ingredientes) {
+		super(nome, preco, descricao, estoque, fabricante);
+		this.dataValidade = dataValidade;
+	    this.ingredientes = ingredientes;
+	  }
+	
+	public String getDataValidade() {
+		return dataValidade;
+	}
+	public void setDataValidade(String datVal) {
+		this.dataValidade = datVal;
+	}
+	public String getIngredientes() {
+		return ingredientes;
+	}
+	public void setIngredientes(String ingredientes) {
+		this.ingredientes = ingredientes;
+	}
+
+	
+	
+}
---
+++ Eletronico.java
@@ -0,0 +1,14 @@
+package Model;
+
+public class Eletronico extends Produto{
+	private String modelo;
+	
+	public Eletronico(String nome, double preco, String descricao, int estoque, String fabricante, String modelo) {
+		super(nome, preco, descricao, estoque, fabricante);
+		this.modelo = modelo;
+    }
+	
+	public String getModelo() {
+        return modelo;
+    }
+}
